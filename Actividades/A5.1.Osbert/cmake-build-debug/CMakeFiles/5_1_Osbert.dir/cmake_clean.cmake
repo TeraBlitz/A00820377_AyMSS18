154ce198b12522d4eee08cc0b65c8392a08d5ef7
terablitz <raulspam96@gmail.com>
2018-02-07 10:00:26 -0600

    avance

---
+++ Actividades/A5.1.Osbert/cmake-build-debug/CMakeFiles/5_1_Osbert.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/5_1_Osbert.dir/main.cpp.obj"
+  "5_1_Osbert.pdb"
+  "5_1_Osbert.exe"
+  "5_1_Osbert.exe.manifest"
+  "lib5_1_Osbert.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_1_Osbert.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Actividades/A5.1.Osbert/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Painting
+{
+public:
+    string artist;
+    int yearPainted;
+    int price;
+    int yearBought;
+};
+
+class Buy{
+public:
+    string sellerName;
+    string sellerAdress;
+    int priceSold;
+    Painting paintingSold;
+};
+
+class Spy
+{
+    Painting wanted[];
+    Painting findPaiting(int year
+
+};
+int main() {
+
+    return 0;
+}
