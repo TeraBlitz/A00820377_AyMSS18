2469b80255530dec771fdee0407eda8d938bc09f
terablitz <raulspam96@gmail.com>
2018-04-11 17:37:28 -0500

    tarea

--- Evaluaciones/E2.2.Parcial2/main.cpp
+++ Evaluaciones/E2.2.Parcial2/main.cpp
@@ -2,28 +2,83 @@
 #include <vector>
 #include <algorithm>
 
-
 using namespace std;
 
 
 class Visitor;
+class GameInformer
+{
+public:
+    virtual void update(string, VG*) = 0;
+};
 
 
 class VG{
 public:
     string title;
     int price;
-
+    vector<GameInformer*> observers;
     virtual VG* clone()=0;
     VG(){}
-    void Concepcion();
-    void Diseno();
-    void Planificacion();
-    void Produccion();
-    void Pruebas();
+    void Concepcion()
+    {
+        cout<<title<<" esta en concepcion..."<<endl;
+    }
+    void Diseno(){
+        cout<<title<<" esta en diseno..."<<endl;
+    }
+    void Planificacion(){
+
+        cout<<title<<" esta en planificacion..."<<endl;
+    }
+    void Produccion(){
+        cout<<title<<" esta en produccion..."<<endl;
+    }
+    void Pruebas(){
+        cout<<title<<" esta en pruebas..."<<endl;
+    }
+    virtual void accept(IVisitor*) = 0;
+    void attach(GameInformer* o){
+        observers.push_back(o);
+    }
+    void dettach(GameInformer* o)
+    {
+        for(int i = 0; i < observers.size();i++)
+        {
+            if(observers[i] == o)
+            {
+                observers.erase(observers.begin()+i);
+                return;
+            }
+        }
+    }
+    void notifyAll(VG* vg)
+    {
+        for(int i = 0; i < observers.size(); i++)
+        {
+            observers[i]->update("game out ", vg);
+        }
+    }
+    bool operator < (const VG& cp) const
+    {
+        return (price < cp.price);
+    }
+    bool operator > (const VG& cp) const
+    {
+        return (price > cp.price);
+    }
 };
 
-class Strategy:public VG{
+template<class SubClass>
+class CloneCRTP: public VG{
+public:
+    VG* clone()
+    {
+        return new SubClass(dynamic_cast<SubClass&> (*this));
+    }
+};
+
+class Strategy: public CloneCRTP<Strategy>{
 public:
     int levels;
     string dificulty;
@@ -33,7 +88,7 @@ public:
         levels=l;
         dificulty=d;
         bois = b;
-
+        notifyAll(this);
     }
     VG* clone() {
         return new Strategy(*this);
@@ -41,10 +96,12 @@ public:
     Strategy(const VG& temp) {
         title = (temp.title);
         price = (temp.price);
+        notifyAll(this);
     }
+    void accept(IVisitor* visitor);
 };
 
-class Adventure:public VG{
+class Adventure: public CloneCRTP<Adventure>{
 public:
     int world;
     string stas;
@@ -54,18 +111,21 @@ public:
         world=l;
         stas=d;
         girls = b;
-
+        notifyAll(this);
     }
     VG* clone() {
         return new Adventure(*this);
+        notifyAll(this);
     }
     Adventure(const VG& temp) {
         title = (temp.title);
         price = (temp.price);
+        notifyAll(this);
     }
+    void accept(IVisitor* visitor);
 };
 
-class Aprendizaje :public VG{
+class Aprendizaje : public CloneCRTP<Aprendizaje>{
 public:
     int learn;
     string much;
@@ -75,7 +135,7 @@ public:
         learn=l;
         much=d;
         pan = b;
-
+        notifyAll(this);
     }
     VG* clone() {
         return new Aprendizaje (*this);
@@ -83,14 +143,37 @@ public:
     Aprendizaje (const VG& temp) {
         title = (temp.title);
         price = (temp.price);
+        notifyAll(this);
     }
+    void accept(IVisitor* visitor);
 };
 
 class Factory {
+private:
+        static Factory* instance;
+        static int counter;
+public:
     template<class vgt>
     vgt* fMethod(string t, int p) {
         return new vgt(t, p);
     }
+        static Factory* getInstance()
+        {
+            if(instance == nullptr)
+            {
+                instance = new Factory;
+            }
+            return instance;
+        }
+        static void destoryInstance()
+        {
+            if(instance != nullptr && counter == 0)
+            {
+                delete instance;
+                instance = nullptr;
+            }
+    }
+
 
     template<class vgt>
     vgt *execute(string t, int p) {
@@ -125,15 +208,13 @@ public:
         if(t){
             for(int i=0;i<inv.size();i++){
                 if(t){
-                    sort(inv.begin(),inv.end(),[](const VG& a,const VG& b){return a.price<b.price;});
                 }
-
             }
         }
     }
-    void findGame(int id) {
+    void findGame(string title) {
         for(int i=0;i<inv.size();i++){
-            if(inv[i]->title==id) {
+            if(inv[i]->title==title) {
                 inv.erase(inv.begin()+i-1);
             }
         }
@@ -151,10 +232,7 @@ public:
 
 };
 
-class GameInformer
-
 
 int main(){
-   
-    return 0;
+
 }
